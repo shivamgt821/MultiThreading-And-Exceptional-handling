aa7f43cb0a8448aa61eba86ff31a857ee7783015
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-21 22:42:15 +0530

    Create Demo4.java

---
+++ Demo4.java
@@ -0,0 +1,20 @@
+public class Demo4 {
+
+	public static void main(String[] args) {
+		System.out.println("main start");
+		try {
+		System.out.println(10/0);  //  first we got an exception -->  Exception in thread "main" java.lang.ArithmeticException: / by zero
+		} 
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array block");
+		}
+		catch(ArithmeticException e) { // then we use try and catch block to handled that exception
+			System.out.println("Arth block");
+		}
+		catch(Exception e) {
+			System.out.println("Exception");
+		}
+		System.out.println("main end");
+	}
+
+}
