d6141149ee3c93bc8903bed152f1a9a7814b0a53
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-26 22:50:24 +0530

    Create Ducking_Exception_Throws.java

---
+++ Ducking_Exception_Throws.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Demo01 {
+	void fun() throws Exception {
+		System.out.println("Connection 2 is establised");
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the numerator");
+		int a = scn.nextInt();
+		System.out.println("Enter the denominator");
+		int b = scn.nextInt();
+		int c = a/b;
+		System.out.println(c);
+		System.out.println("Connection 2 is terminated");
+	}
+}
+public class Ducking_Exception_Throws {
+
+	public static void main(String[] args) {
+		System.out.println("Connection 1 is established");
+		try {
+			Demo01 d1 = new Demo01();
+			d1.fun();
+		}
+		catch(Exception e) {
+			System.out.println("Exception handled in main()");
+		}
+		System.out.println("Connection 1 is terminated");
+
+	
+	}
+
+}
