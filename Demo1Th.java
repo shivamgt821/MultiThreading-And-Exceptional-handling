170bcacbc951fff46a787960f3995fce8d551524
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-06-29 13:01:02 +0530

    Create Demo1Th.java

---
+++ Demo1Th.java
@@ -0,0 +1,12 @@
+public class Demo1Th {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("main start");
+		Thread.sleep(2000);
+		System.out.println("main execute");
+		Thread.sleep(2000);
+		System.out.println("main end");
+		Thread.sleep(2000);
+	}
+
+}
