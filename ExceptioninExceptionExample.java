7f326d11eadeda388b2170fc51348bb2494a5c8e
SHIVAM KUMAR GUPTA <120335161+shivamgt821@users.noreply.github.com>
2023-07-07 18:22:10 +0530

    Create ExceptioninExceptionExample.java

---
+++ ExceptioninExceptionExample.java
@@ -0,0 +1,20 @@
+public class ExceptioninExceptionExample {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("main start");
+			System.out.println(5/0);
+			System.out.println("main ends");
+		} 
+		catch (ArithmeticException e) {
+			System.out.println("Arithmetic Exception handled");
+		}
+		catch (Exception e) {
+			System.out.println("Exception handled");
+		}
+		catch (ClassNotFoundException e) {   // Unreachable catch block for ClassNotFoundException. It is already handled by the catch block for Exception
+			System.out.println("Class Not FoundException handled");
+		}
+	}
+
+}
